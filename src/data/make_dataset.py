41d7d925373700b05747b1815f2e38cca4dcd4d7
Jakob <accounts@jakoblangenbahn.com>
2022-01-16 13:12:55 +0000

    Make dataset for speeches

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,36 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+""" Functions to create dataset for analysis
+"""
+
+import psycopg2
+import pandas as pd
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+def create_speeches_dataset(con_details, query, save_location):
+    """ Use local Docker database to extract speeches data
+
+    :param con_details: Connection details for database
+    :param query: SQL query for data extraction
+    :param save_location: Location for resulting files
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    con = psycopg2.connect(**con_details)
+    cur = con.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    df = pd.DataFrame(rows)
+    column_names = ["id", "session", "electoral_term", "first_name", "last_name", "politician_id", "speech_content",
+                    "fraction_id", "document_url", "position_short", "position_long", "date", "search_speech_content"]
+    df.columns = column_names
+    df.to_csv(save_location, index=False)
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    con_details_localhost = {
+        "host": "localhost",
+        "database": "next",
+        "user": "postgres",
+        "password": "postgres",
+        "port": "5432"
+    }
+    query_all = """SELECT * from open_discourse.speeches WHERE electoral_term = 18 OR electoral_term = 19"""
+    save_location_all = "../../data/interim/bundestag_speeches_processed.csv"
+    create_speeches_dataset(con_details_localhost, query_all, save_location_all)
